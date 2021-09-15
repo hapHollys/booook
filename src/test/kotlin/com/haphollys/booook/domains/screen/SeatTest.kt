4c0988ddd591188b2e39e1aa05ceaa0731807db8
JJY <galid1@naver.com>
2021-09-15 09:09:18 +0900

    test: Seat 테스트 코드 추가

---
+++ src/test/kotlin/com/haphollys/booook/domains/screen/SeatTest.kt
@@ -0,0 +1,83 @@
+package com.haphollys.booook.domains.screen
+
+import com.haphollys.booook.domains.screen.Seat.SeatStatus.BOOKED
+import com.haphollys.booook.domains.screen.Seat.SeatStatus.FREE
+import com.haphollys.booook.domains.screen.Seat.SeatType.FRONT
+import com.haphollys.booook.model.SeatPosition
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+
+internal class SeatTest {
+    @Test
+    fun `좌석 예약`() {
+        // given
+        val seat = Seat(
+            seatPosition = SeatPosition(
+                x = 0,
+                y = 0,
+            ),
+            seatType = FRONT
+        )
+
+        // when
+        seat.book()
+
+        // then
+        assertEquals(BOOKED, seat.status)
+    }
+
+    @Test
+    fun `좌석 예약 취소`() {
+        // given
+        val seat = Seat(
+            seatPosition = SeatPosition(
+                x = 0,
+                y = 0,
+            ),
+            seatType = FRONT
+        )
+        seat.book()
+
+        // when
+        seat.unBook()
+
+        // then
+        assertEquals(FREE, seat.status)
+    }
+
+    @Test
+    fun `이미 예약된 좌석은 예약할 수 없다`() {
+        // given
+        val bookedSeat = Seat(
+            seatPosition = SeatPosition(
+                x = 0,
+                y = 0,
+            ),
+            seatType = FRONT,
+            status = BOOKED
+        )
+
+        assertThrows(RuntimeException::class.java) {
+            bookedSeat.book()
+        }
+    }
+
+    @Test
+    fun `예약되지 않은 좌석 예약 취소시 예외`() {
+        // given
+        val freeSeat = Seat(
+            seatPosition = SeatPosition(
+                x = 0,
+                y = 0
+            ),
+            seatType = FRONT,
+            status = FREE
+        )
+
+        // when, then
+        assertThrows(RuntimeException::class.java) {
+            freeSeat.unBook()
+        }
+    }
+}
