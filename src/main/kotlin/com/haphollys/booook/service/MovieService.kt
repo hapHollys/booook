8d3ff96f0da151132a105099342b34beb5acdeab
JJY <galid1@naver.com>
2021-10-19 19:30:49 +0900

    modify: 성능 테스트를 위해 readOnly 설정 롤백

--- src/main/kotlin/com/haphollys/booook/service/MovieService.kt
+++ src/main/kotlin/com/haphollys/booook/service/MovieService.kt
@@ -7,7 +7,7 @@ import org.springframework.transaction.annotation.Transactional
 import javax.persistence.EntityNotFoundException
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 class MovieService(
     private val movieRepository: MovieRepository
 ) {
